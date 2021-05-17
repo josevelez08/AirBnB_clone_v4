d813d6339c07094a89f04739fb6cbf60a8bab9fd
klich1984 <2239@holbertonschool.com>
2021-05-16 22:08:48 -0500

    delete coments

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -3,8 +3,6 @@ const d = document;
 const dictAdd = {};
 $(() => {
 	$(d).on("click", "input[type=checkbox]", function () {
-		// if (this.checked) dictAdd[this.dataset.id] = this.dataset.name;
-		// else delete dictAdd[this.dataset.id];
 		this.checked
 			? (dictAdd[this.dataset.id] = this.dataset.name)
 			: delete dictAdd[this.dataset.id];
@@ -16,8 +14,6 @@ $(() => {
 	// cambiar por http://0.0.0.0:5001/api/v1/status/
 	$.get("http://localhost:5001/api/v1/status/", function (data) {
 		$queryId = d.getElementById("api_status");
-		// if (data.status === "OK") $queryId.classList.add("available");
-		// else $queryId.classList.remove("available");
 		data.status === "OK"
 			? $queryId.classList.add("available")
 			: $queryId.classList.remove("available");
