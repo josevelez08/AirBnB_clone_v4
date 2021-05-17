ec36502fe3e9a782fd15196c776196a022d7686b
klich1984 <2239@holbertonschool.com>
2021-05-16 22:07:34 -0500

    delete coments and change if else for ternary operator

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -3,16 +3,11 @@ const d = document;
 const dictAdd = {};
 $(() => {
 	$(d).on("click", "input[type=checkbox]", function () {
-		// console.log(this)
-		// console.log(this.checked)
-		if (this.checked) {
-			// console.log("segundo this", this.dataset)
-			dictAdd[this.dataset.id] = this.dataset.name;
-			// console.log("dict =", dictAdd);
-		} else {
-			delete dictAdd[this.dataset.id];
-			// console.log("dict =", dictAdd);
-		}
+		// if (this.checked) dictAdd[this.dataset.id] = this.dataset.name;
+		// else delete dictAdd[this.dataset.id];
+		this.checked
+			? (dictAdd[this.dataset.id] = this.dataset.name)
+			: delete dictAdd[this.dataset.id];
 		$(".amenities h4").text(Object.values(dictAdd).join(", "));
 	});
 });
@@ -20,15 +15,11 @@ $(() => {
 $(() => {
 	// cambiar por http://0.0.0.0:5001/api/v1/status/
 	$.get("http://localhost:5001/api/v1/status/", function (data) {
-		console.log("data =", data)
-		// console.log(d)
-		$get_id = d.getElementById("api_status");
-		// console.log($get_id);
-		if (data.status === "OK") {
-      $get_id.classList.add("available");
-			// console.log("Entre");
-		} else {
-      $get_id.classList.remove("available");
-    }
+		$queryId = d.getElementById("api_status");
+		// if (data.status === "OK") $queryId.classList.add("available");
+		// else $queryId.classList.remove("available");
+		data.status === "OK"
+			? $queryId.classList.add("available")
+			: $queryId.classList.remove("available");
 	});
 });
